755bcb0a889ab8d8e4f5eb63d08bc2010c861039
Livnoni <yehuda.livnoni@gmail.com>
2020-12-23 17:21:54 +0200

    Update ect.tf

--- aws/ect.tf
+++ aws/ect.tf
@@ -4,6 +4,7 @@ resource "aws_instance" "web_host" {
   instance_type = "t2.nano"
 
   
+  
   vpc_security_group_ids = [
   "${aws_security_group.web-node.id}"]
   subnet_id = "${aws_subnet.web_subnet.id}"
