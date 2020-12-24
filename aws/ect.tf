965656af5b69b4d0177303ea2aa9a3c265205cba
Livnoni <yehuda.livnoni@gmail.com>
2020-12-24 12:32:35 +0200

    Update ect.tf

--- aws/ect.tf
+++ aws/ect.tf
@@ -1,15 +1,18 @@
 resource "aws_instance" "web_host" {
   # ec2 have plain text secrets in user data
   ami           = "${var.ami}"
+  
   instance_type = "t2.nano"
 
   
   
   vpc_security_group_ids = [
   "${aws_security_group.web-node.id}"]
+  
   subnet_id = "${aws_subnet.web_subnet.id}"
   user_data = <<EOF
 #! /bin/bash
+
 sudo apt-get update
 sudo apt-get install -y apache2
 sudo systemctl start apache2
@@ -17,28 +20,37 @@ sudo systemctl enable apache2
 export AWS_ACCESS_KEY_ID=AKIAIOSFODNN7EXAMAAA
 export AWS_SECRET_ACCESS_KEY=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
 export AWS_DEFAULT_REGION=us-west-2
+
 echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
 EOF
   tags = {
     Name = "${local.resource_prefix.value}-ec2"
+    
   }
 }
 
 resource "aws_ebs_volume" "web_host_storage" {
+  
   # unencrypted volume
+  
   availability_zone = "${var.availability_zone}"
   #encrypted         = false  # Setting this causes the volume to be recreated on apply 
   size              = 1
+  
   tags = {
     Name = "${local.resource_prefix.value}-ebs"
+    
   }
 }
 
 resource "aws_ebs_snapshot" "example_snapshot" {
+  
   # ebs snapshot without encryption
   volume_id   = "${aws_ebs_volume.web_host_storage.id}"
   description = "${local.resource_prefix.value}-ebs-snapshot"
+  
   tags = {
     Name = "${local.resource_prefix.value}-ebs-snapshot"
+    
   }
 }
